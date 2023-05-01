31ff085587220618b8d9c8e79add23f576b2ff59
RBelachew <49268743+RBelachew@users.noreply.github.com>
2023-05-01 15:28:26 +0300

    Add files via upload

---
+++ React_JobBot/src/pages/chatBotLogic/components/Options/MoreInfo.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {useState,useEffect} from "react";
+
+import "./Options.css";
+
+const MoreInfo = (props) => {
+
+    function moreInfo(){
+        return (
+            <div>
+                For more information click <a href="../../../homePage">here</a>
+            </div>
+            );
+    }
+
+    function selfJobSearch(){
+        return (
+            <div>
+                To self job search click <a href="/jobs">here</a>
+            </div>
+            );
+    }
+
+    return (
+        (props.node.getSelected().field==="Other")?moreInfo():selfJobSearch()
+    );
+};
+
+export default MoreInfo;
+
