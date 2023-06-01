b299ebe2f78beab8502b47931bfd96fb7dc26159
RBelachew <49268743+RBelachew@users.noreply.github.com>
2023-06-01 19:39:44 +0300

    Add files via upload

---
+++ React_JobBot/src/pages/chatBotLogic/components/Options/ErrorMessages.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import "./Options.css";
+
+const ErrorMessages = (props) => {
+    return (
+        <div>
+            Server down. Please refresh the page. Thank you for your patience.
+        </div>
+    );
+};
+
+export default ErrorMessages;
+
