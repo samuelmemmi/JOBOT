eaa3dd0333b434908dc91c6bb8d4f8a0cc2ba4f3
RBelachew <49268743+RBelachew@users.noreply.github.com>
2023-06-03 22:36:24 +0300

    Delete React_JobBot/src directory

--- React_JobBot/src/pages/chatBotLogic/components/Options/DisplaySelectedJobs.test.js
+++
@@ -1,9 +0,0 @@
-// import {filterObjectsById} from "./DisplaySelectedJobs"
-
-// test('Checking filterObjectsById with empty object', () => {
-//     expect(filterObjectsById({}, 0)).toBe([]);
-// })
-
-// test('Checking filterObjectsById with empty object', () => {
-//     expect(filterObjectsById({}, 0)).toBe([]);
-// })
